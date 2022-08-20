5034dca5fc8a05b815b7f9677b6d73f1913d77c7
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-08-20 21:39:42 +0800

    this is demo of push  sample first and second script

--- create.s.node.sh
+++ create.s.node.sh
@@ -16,7 +16,7 @@ create_ec2() {
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-
+       echo server ip address = ${private_ip}
 
 }
 
