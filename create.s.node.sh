f488585765f9b0920e8f396834115c9cb0f54677
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-09-29 13:59:38 +0800

    this is demo of push  sample first and second script

--- create.s.node.sh
+++ create.s.node.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 ##### Change these values ###
-
-ZONE_ID="Z0219582YG9Z5DSKRZHE"
+ZONE_ID="Z05563911B5TPM0CPTZND"
 SG_NAME="allow-all"
 ENV="dev"
 #############################
 
 
-
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
-      --instance-type t3.micro \
+      --instance-type t2.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
+      --iam-instance-profile Name=SecretManager_Role_for_RoboShop_Nodes \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-        echo Server IP Address = ${PRIVATE_IP}
-
+      echo Server IP Address = ${PRIVATE_IP}
 }
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 if [ -z "$1" ]; then
-  echo "input the node name"
+  echo "Input the node name"
   exit 1
 else
   COMPONENT=$1
   create_ec2
 fi
+
