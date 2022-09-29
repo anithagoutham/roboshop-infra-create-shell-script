f8fd9ce49920502976168ba4308ce35f71856a0f
pallas2812 <56209434+pallas2812@users.noreply.github.com>
2022-09-29 13:52:10 +0800

    this is demo of push  sample first and second script

--- create.s.node.sh
+++ create.s.node.sh
@@ -4,7 +4,7 @@
 
 ZONE_ID="Z0219582YG9Z5DSKRZHE"
 SG_NAME="allow-all"
-env="dev"
+ENV="dev"
 #############################
 
 
@@ -13,7 +13,7 @@ create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
@@ -28,6 +28,6 @@ if [ -z "$1" ]; then
   echo "input the node name"
   exit 1
 else
-  COMPONENT=$1
+  component=$1
   create_ec2
 fi
--- create.sh
+++ create.sh
@@ -7,17 +7,17 @@ ENV="dev"
 #############################
 
 
-COMPONENT=all
+component=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t2.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=Yes}]" \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}, {Key=Monitor,Value=Yes}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/component/${component}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
@@ -27,7 +27,7 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NA
 
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do
-  COMPONENT="${component}-${ENV}"
+  component="${component}-${ENV}"
   create_ec2
 done
 
